ebf68dbe9f57b5867e77cef3b12427d81380ffd4
alexamen9 <ameneyroalex@gmail.com>
2023-12-19 00:22:03 -0600

    solving error

--- src/index.js
+++ src/index.js
@@ -11,24 +11,23 @@ import { open } from "sqlite";
 //adding cluster to run multiple instances of nodejs 
 import { availableParallelism } from "node:os";
 import cluster from "node:cluster";
-import { createAdapter,setupPrimary } from "@socket.io/cluster-adapter";
+import { createAdapter, setupPrimary } from "@socket.io/cluster-adapter";
 
-if(cluster.isPrimary){
+if (cluster.isPrimary) {
     const numCPUs = availableParallelism();
-    for(let i = 0; i<numCPUs;i++){
+    for (let i = 0; i < numCPUs; i++) {
         cluster.fork({
             PORT: 3000 + i
         });
     }
     setupPrimary();
-}
+} else {
+    const db = await open({
+        filename: 'chat.db',
+        driver: sqlite3.Database
+    });
 
-const db = await open({
-    filename:'chat.db',
-    driver: sqlite3.Database
-});
-
-await db.exec(`
+    await db.exec(`
     CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         client_offset TEXT UNIQUE,
@@ -36,62 +35,63 @@ await db.exec(`
     );
 `);
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-    connectionStateRecovery: {},
-    adapter: createAdapter()
-});
-const port = process.env.PORT ?? 3000;
-const __dirname = dirname(fileURLToPath(import.meta.url));
+    const app = express();
+    const server = createServer(app);
+    const io = new Server(server, {
+        connectionStateRecovery: {},
+        adapter: createAdapter()
+    });
+    const port = process.env.PORT ?? 3000;
+    const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(morgan("tiny"))
+    app.use(morgan("tiny"))
 
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-});
-
-io.on("connection", async (socket) => {
-
-    socket.on("clientChatMessage", async (msg,clientOffset,callback) => {
-        let result;
-        try {
-            result = await db.run("INSERT INTO messages (content,client_offset) VALUES (?,?)",msg,clientOffset)
-        } catch (error) {
-            console.log(error)
-            if (error.errno === 19 /* SQLITE_CONSTRAINT */ ) {
-                // the message was already inserted, so we notify the client
-                callback();
-              } else {
-                // nothing to do, just let the client retry
-              }
-              return;
-        }
-        io.emit("serverChatMessage", msg, result.lastID);
-        //acknowledge the event
-        callback();
+    app.get('/', (req, res) => {
+        res.sendFile(join(__dirname, 'index.html'));
     });
 
-    if(!socket.recovered) {
-        try {
-            await db.each("SELECT id, content FROM messages WHERE id > ?",[socket.handshake.auth.serverOffset || 0],
-                (_err,row) => {
-                    socket.emit("serverChatMessage",row.content,row.id)
+    io.on("connection", async (socket) => {
+
+        socket.on("clientChatMessage", async (msg, clientOffset, callback) => {
+            let result;
+            try {
+                result = await db.run("INSERT INTO messages (content,client_offset) VALUES (?,?)", msg, clientOffset)
+            } catch (error) {
+                console.log(error)
+                if (error.errno === 19 /* SQLITE_CONSTRAINT */) {
+                    // the message was already inserted, so we notify the client
+                    callback();
+                } else {
+                    // nothing to do, just let the client retry
                 }
-            )
-        } catch (error) {
-            console.log(error);
+                return;
+            }
+            io.emit("serverChatMessage", msg, result.lastID);
+            //acknowledge the event
+            callback();
+        });
+
+        if (!socket.recovered) {
+            try {
+                await db.each("SELECT id, content FROM messages WHERE id > ?", [socket.handshake.auth.serverOffset || 0],
+                    (_err, row) => {
+                        socket.emit("serverChatMessage", row.content, row.id)
+                    }
+                )
+            } catch (error) {
+                console.log(error);
+            }
         }
-    }
 
-    socket.on("disconnect",()=>{
-        console.log("user disconnected");
-    })
-});
-
-
-server.listen(port, () => {
-    console.log(`running on server: ${port}`);
-});
+        socket.on("disconnect", () => {
+            console.log("user disconnected");
+        })
+    });
 
 
+    server.listen(port, () => {
+        console.log(`running on server: ${port}`);
+    });
+
+
+}
